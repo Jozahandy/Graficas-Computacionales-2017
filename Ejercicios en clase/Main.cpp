75ffed0b15cdeac16b45ac72a8498bf22f91f9b2
VAL\Valery <valeryy96@gmail.com>
2017-10-20 08:25:12 -0500

    Examen segundo parcial Gráficas computacionales Grupo 2

--- Ejercicios en clase/Main.cpp
+++ Ejercicios en clase/Main.cpp
@@ -1,11 +1,11 @@
 /*********************************************************
 Materia:Gráficas computacionales
-Fecha: 16/Octubre/2017
+Fecha: 23/Octubre/2017
+EXAMEN SEGUNDO PARCIAL 
 Autor: Valeria Jozahandy Sánchez Álvarez
-		Diana Isabel Hernández María
-Matricula: A01375042
+	   Diana Isabel Hernández María
+Matricula: A01375042, A01375351
 *********************************************************/
-
 
 #include <iostream>
 #include <GL/glew.h>
@@ -20,116 +20,50 @@ Matricula: A01375042
 Mesh _mesh;
 ShaderProgram _shaderProgram;
 Transform _transform;
+Transform _transform2;
 Camera _camera;
+float contador = 0.0f;
+float contador2 = 0.0f;
+float contador3 = 0.0;
+
 
 void Initialize()
 {
 	std::vector<glm::vec3> positions;
 
-	//Cara Frente 
-	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f)); //0
-	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f)); //1
-	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f)); //2
-	positions.push_back(glm::vec3(-3.0, 3.0f, 3.0f)); //3
-
-	//Cara Derecha
-	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f)); //4
-	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f)); //5
-	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f)); //6
-	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f)); //7
-
-	//Cara Izquierda 
-	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f)); //8
-	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f)); //9
-	positions.push_back(glm::vec3(-3.0f, 3.0f, 3.0f)); //10
-	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f)); //11
-
-	//Cara Atras 
-	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f)); //12
-	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f)); //13
-	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f)); //14
-	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f)); //15
-
-	//Cara Arriba 
-	positions.push_back(glm::vec3(-3.0f, 3.0f, 3.0f)); //16
-	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f)); //17
-	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f)); //18
-	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f)); //19
-														
-	//Cara Abajo
-	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f)); //20
-	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f)); //21
-	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f)); //22
-	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f)); //23
-
+	//Base 
+	positions.push_back(glm::vec3(-1.0f, -1.0f, -1.0f)); //0
+	positions.push_back(glm::vec3(1.0f, -1.0f, -1.0f)); //1
+	positions.push_back(glm::vec3(1.0f, -1.0f, 1.0f)); //2
+	positions.push_back(glm::vec3(-1.0f, -1.0f, 1.0f)); //3
+														//Pico 
+	positions.push_back(glm::vec3(0.0f, 1.0f, 0.0f)); //4
 
 	std::vector<glm::vec3> colors;
-	//Cara Frente 
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	
-	//Cara Derecha
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	
-	//Cara Izquierda 
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	
-	//Cara Atras
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	
-	//Cara Arriba
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	
-	//Cara Abajo
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f)); //0
+	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f)); //1
+	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f)); //2
+	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f)); //3
+	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f)); //4
 
 	std::vector<unsigned int> indices;
-	//Cara Frente 
+	//Base 
 	indices.push_back(0); indices.push_back(1); indices.push_back(2);
 	indices.push_back(0); indices.push_back(2); indices.push_back(3);
-	
-	//Cara Derecha
-	indices.push_back(4); indices.push_back(5); indices.push_back(6);
-	indices.push_back(4); indices.push_back(6); indices.push_back(7);
-	
+	//Cara Frente
+	indices.push_back(3); indices.push_back(2); indices.push_back(4);
+	//Cara Derecha 
+	indices.push_back(2); indices.push_back(1); indices.push_back(4);
 	//Cara Izquierda
-	indices.push_back(8); indices.push_back(9); indices.push_back(10);
-	indices.push_back(8); indices.push_back(10); indices.push_back(11);
-	
-	//Cara Atras
-	indices.push_back(12); indices.push_back(13); indices.push_back(14);
-	indices.push_back(12); indices.push_back(14); indices.push_back(15);
-	
-	//Cara Arriba
-	indices.push_back(16); indices.push_back(17); indices.push_back(18);
-	indices.push_back(16); indices.push_back(18); indices.push_back(19);
-	
-	//Cara Abajo
-	indices.push_back(20); indices.push_back(21); indices.push_back(22);
-	indices.push_back(20); indices.push_back(22); indices.push_back(23);
+	indices.push_back(0); indices.push_back(3); indices.push_back(4);
+	//Cara Atras 
+	indices.push_back(1); indices.push_back(0); indices.push_back(4);
 
-	_mesh.CreateMesh(24);
+	_mesh.CreateMesh(5);
 	_mesh.SetIndices(indices, GL_STATIC_DRAW);
 	_mesh.SetPositionAttribute(positions, GL_STATIC_DRAW, 0);
 	_mesh.SetColorAttribute(colors, GL_STATIC_DRAW, 1);
-
 
 
 	_shaderProgram.CreateProgram();
@@ -139,28 +73,64 @@ void Initialize()
 	_shaderProgram.SetAttribute(1, "VertexColor");
 	_shaderProgram.LinkProgram();
 
-	//_transform.SetRotation(0.0f, 0.0f, 90.0f);
+	_transform.SetScale(3.0f, 3.0f, 3.0f); //Geometría 1
 
-	//_camera.SetOrthographic(1.0f, 1.0f); Esto convierte la cámara en 2D
+	_transform2.SetPosition(0.0f, 0.0f, 0.0f); //Geometría2
+	_transform2.SetScale(0.5f, 0.5f, 0.5f); //Geometría2 
+
+
+											//_camera.SetOrthographic(1.0f, 1.0f); Esto convierte la cámara en 2D
 }
 
 void GameLoop()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpiamos el buffer de control y el de profundidad. Siempre hacerlo al inicio del frame
 
-	_camera.SetPosition(0.0f, 0.0f, 15.0f);
+	_camera.SetPosition(0.0f, 0.0f, 25.0f);
 
-	_transform.Rotate(0.01f, 0.01f, 0.01f, true); // si es false es rotación local, si es true es rotación del mundo 
+	//Geometría 1
+	_transform.Rotate(0.01f, 0.01f, 0.01f, true);
+	float radio = 5.0f;
+	float angulo = glm::radians(contador);
+	float x = radio*(glm::cos(angulo));
+	float y = radio*(glm::sin(angulo));
+	float z = 0;
+
+	_transform.SetPosition(x, y, z);
+	contador = contador + 0.005f;
+
+
+	//Geometría 2 
+	_transform2.Rotate(-0.01f, -0.01f, -0.01f, true);
+
+	float escala = 0.5f + contador2;
+
+	if (escala <= 1.0f & contador3 == 0.0f) {
+		_transform2.SetScale(escala, escala, escala);
+		contador2 = contador2 + 0.00005f;
+	}
+	else if (escala >= 1.0f & contador3 == 0.0f) {
+		contador3 = 1.0f;
+	}
+	else if (escala >= 0.25f & contador3 == 1.0f) {
+		_transform2.SetScale(escala, escala, escala);
+		contador2 = contador2 - 0.00005f;
+	}
+	else if (escala <= 0.25f & contador3 == 1.0f) {
+		contador3 = 0.0f;
+	}
 
 	_shaderProgram.Activate();
+
 	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection()* _transform.GetModelMatrix());
+	_mesh.Draw(GL_TRIANGLES); //Geometría 1
 
+	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection()* _transform2.GetModelMatrix());
+	_mesh.Draw(GL_TRIANGLES); //Geometría 2
 
-	_mesh.Draw(GL_TRIANGLES);
 	_shaderProgram.Deactivate();
 
-	//Cuando terminamos de renderear cambiamos los buffers 
-	glutSwapBuffers();
+	glutSwapBuffers(); //Cuando terminamos de renderear cambiamos los buffers 
 }
 
 void Idle()
